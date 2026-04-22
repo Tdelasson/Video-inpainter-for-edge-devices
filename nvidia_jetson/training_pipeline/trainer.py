96becba3995d62be68e490c1d285cb52b4956a6f
TobiasRDL <tlasso23@student.aau.dk>
2026-04-22 13:01:05 +0200

    Added clipping to gradients to try to fix black dots resulting from the GRU

--- nvidia_jetson/training_pipeline/trainer.py
+++ nvidia_jetson/training_pipeline/trainer.py
@@ -149,6 +149,9 @@ def train(args, model, flow_model, discriminator, train_loader, mask_dataset, op
                     discriminator=discriminator, fake_seq=fake_seq
                 )
                 total_loss.backward()
+
+                torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
+
                 optimizer_model.step()
 
                 # Step Schedulers
