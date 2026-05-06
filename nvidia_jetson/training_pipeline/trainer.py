f3b976deb5e5fdfdfe24047554ca0ae8b8b10bb6
TobiasRDL <tlasso23@student.aau.dk>
2026-05-06 14:38:20 +0200

    Increase discriminator learning rate for improved training dynamics

--- nvidia_jetson/training_pipeline/trainer.py
+++ nvidia_jetson/training_pipeline/trainer.py
@@ -357,7 +357,7 @@ def main():
 
     # Optimizers
     opt_model = optim.Adam(model.parameters(), lr=args.lr)
-    opt_disc = optim.Adam(discriminator.parameters(), lr=args.lr * 1.0)
+    opt_disc = optim.Adam(discriminator.parameters(), lr=args.lr * 4.0)
 
     scheduler_model = CosineAnnealingLR(opt_model, T_max=args.iterations, eta_min=1e-5)
     scheduler_disc = CosineAnnealingLR(opt_disc, T_max=args.iterations, eta_min=1e-6)
