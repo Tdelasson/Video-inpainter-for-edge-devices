4c9f06f159ce009a39d4180479b86f0327365ba5
TobiasRDL <tlasso23@student.aau.dk>
2026-05-07 12:57:49 +0200

    Adjust noise standard deviation calculation in trainer.py for improved stability

--- nvidia_jetson/training_pipeline/trainer.py
+++ nvidia_jetson/training_pipeline/trainer.py
@@ -208,7 +208,7 @@ def train(args, model, flow_model, discriminator, train_loader, val_loader, mask
                 real_seq = torch.cat([real_pixel_seq, mask_seq], dim=1)
 
                 noise_decay_iters = args.iterations * 0.75
-                current_sigma = max(0.0, 0.02 * (1.0 - (current_iter / noise_decay_iters)))
+                current_sigma = max(0.0, 0.00 * (1.0 - (current_iter / noise_decay_iters)))
                 discriminator.set_std(current_sigma)
 
                 optimizer_disc.zero_grad()
