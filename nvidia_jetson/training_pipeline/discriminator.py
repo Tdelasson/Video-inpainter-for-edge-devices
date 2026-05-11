4afbba0b331f0915169cb5b0cab47e82aab66271
TobiasRDL <tlasso23@student.aau.dk>
2026-05-11 17:21:49 +0200

    Refactor discriminator to use VideoPatchGAN and streamline loss calculations

--- nvidia_jetson/training_pipeline/discriminator.py
+++ nvidia_jetson/training_pipeline/discriminator.py
@@ -15,13 +15,15 @@ class NoisyDiscriminator(nn.Module):
             x = x + torch.randn_like(x) * self.current_std
         return self.discriminator(x)
 
+
 class VideoPatchGAN(nn.Module):
     def __init__(self, in_channels=4, ndf=64, n_layers=3):
         super().__init__()
-        kw = 4
-        padw = 1
+        # Use temporal kernel 3 and padding 1 to perfectly preserve sequence length
+        kw = (3, 4, 4)
+        padw = (1, 1, 1)
 
-        # Strided purely spatially to maintain temporal resolution (or use (2,2,2) to compress time)
+        # Strided purely spatially to maintain temporal resolution
         stride = (1, 2, 2)
 
         sequence = [
@@ -45,7 +47,7 @@ class VideoPatchGAN(nn.Module):
         nf_mult = min(2 ** n_layers, 8)
         sequence += [
             nn.utils.spectral_norm(
-                nn.Conv3d(ndf * nf_mult_prev, ndf * nf_mult, kernel_size=kw, stride=1, padding=padw)
+                nn.Conv3d(ndf * nf_mult_prev, ndf * nf_mult, kernel_size=kw, stride=(1, 1, 1), padding=padw)
             ),
             nn.BatchNorm3d(ndf * nf_mult),
             nn.LeakyReLU(0.2, True)
@@ -53,12 +55,12 @@ class VideoPatchGAN(nn.Module):
 
         # Output 1 channel prediction map
         sequence += [
-            nn.Conv3d(ndf * nf_mult, 1, kernel_size=kw, stride=1, padding=padw)
+            nn.Conv3d(ndf * nf_mult, 1, kernel_size=kw, stride=(1, 1, 1), padding=padw)
         ]
 
         self.model = nn.Sequential(*sequence)
 
     def forward(self, x):
         # x shape: (B, C, T, H, W)
-        # output shape: (B, 1, T', H', W')
+        # output shape: (B, 1, T, H', W')
         return self.model(x)
