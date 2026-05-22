ab4148bc57ff5045be5ea5cb06797ca9ff79d4be
TobiasRDL <tlasso23@student.aau.dk>
2026-05-22 10:45:28 +0200

    Update Viper model and input size in info_test.py for new channel and resolution configuration

--- nvidia_jetson/model_architecture/tests/info_test.py
+++ nvidia_jetson/model_architecture/tests/info_test.py
@@ -3,5 +3,5 @@ from .. import Viper
 from .. import unet_cell
 
 
-model = Viper(in_channels=10, base_channels=128, num_layers=4)
-summary(model, input_size=(1, 10, 512, 512)) # Batch, Channels, H, W
+model = Viper(in_channels=20, base_channels=128, num_layers=4)
+summary(model, input_size=(1, 20, 432, 240)) # Batch, Channels, H, W
